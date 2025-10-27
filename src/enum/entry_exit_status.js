79fbfb64bd1b74ecb3180b41b124e5c7f9fa0a1b
stanislasBeyra <beyrastanislas@gmail.com>
2025-10-27 20:29:35 +0000

    maj

--- src/enum/entry_exit_status.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ExitStatus = void 0;
-var ExitStatus;
-(function (ExitStatus) {
-    ExitStatus["EN_COURS"] = "EN_COURS";
-    ExitStatus["TERMINEE"] = "TERMINEE";
-    ExitStatus["EN_RETARD"] = "EN_RETARD";
-})(ExitStatus || (exports.ExitStatus = ExitStatus = {}));
--- src/enum/userstatus.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserStatus = void 0;
-var UserStatus;
-(function (UserStatus) {
-    UserStatus["PRESENT"] = "PRESENT";
-    UserStatus["SORTIE"] = "SORTIE";
-    UserStatus["RETARD"] = "RETARD";
-})(UserStatus || (exports.UserStatus = UserStatus = {}));
